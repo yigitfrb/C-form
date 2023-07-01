32f93418216aa95fece1c4e07c145a5a07051455
yigit <yigit@DESKTOP-GJB3DJI>
2023-07-01 18:26:03 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SKOR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SORULAR";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(450, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CEVAPLAR";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(453, 219);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "harf al";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(285, 330);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "doğru";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(453, 330);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "yanlış";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 182);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "KALAN SÜRE";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(644, 90);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "baslat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(644, 156);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 28);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "durdur";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(644, 247);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(115, 33);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "SORUYU GEÇ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 432);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public string[] questions;
+        public string[] answers;
+        public int currentQuestionIndex;
+        public int score;
+        public string harf;
+        public string[] harfler;
+        public int indis;
+        public double zaman = 60.0;
+        List<int> previousGuesses = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            questions = new string[]
+            {
+                "What is the capital of France?",
+                "Which country is known as the 'Land of the Rising Sun'?",
+                "What is the tallest mountain in the world?",
+                "How many continents are there?"
+            };
+
+            answers = new string[]
+            {
+                "Paris",
+                "Japan",
+                "mounteverest",
+                "7"
+            };
+
+            
+
+            currentQuestionIndex = 0;
+            score = 0;
+
+
+            // Set up the form
+            label3.Text = questions[currentQuestionIndex];
+            label2.Text = $"Score: {score}";
+            labelaktar();
+        }
+        void labelaktar()
+        {
+            label1.Text = "";
+            for (int i = 0; i < answers[currentQuestionIndex].Length; i++)
+            {
+                label1.Text += "_ ";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    string userAnswer = textBox1.Text.Trim();
+        //    string correctAnswer = answers[currentQuestionIndex];
+
+        //    // Check if the answer is correct
+        //    if (string.Equals(userAnswer, correctAnswer, StringComparison.OrdinalIgnoreCase))
+        //    {
+        //        score+=10*(answers[currentQuestionIndex].Length-previousGuesses.Count());
+        //        currentQuestionIndex++;
+        //        previousGuesses.Clear();
+        //        // Move to the next question or end the game
+        //        if (currentQuestionIndex < questions.Length)
+        //        {
+        //            label3.Text = questions[currentQuestionIndex];
+        //            label2.Text = $"Score: {score}";
+        //            labelaktar();
+        //            textBox1.Text = string.Empty;
+        //        }
+        //        else
+        //        {
+        //            // Show the final score
+        //            label3.Text = "Game Over!";
+        //            label2.Text = $"Final Score: {score}";
+
+        //            textBox1.Enabled = false;
+        //            button1.Enabled = false;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        score-=10;
+        //        // Move to the next question or end the game
+        //        if (currentQuestionIndex < questions.Length)
+        //        {
+        //            label3.Text = questions[currentQuestionIndex];
+        //            label2.Text = $"Score: {score}";
+        //            textBox1.Text = string.Empty;
+        //        }
+        //        else
+        //        {
+        //            // Show the final score
+        //            label3.Text = "Game Over!";
+        //            label2.Text = $"Final Score: {score}";
+
+        //            textBox1.Enabled = false;
+        //            button1.Enabled = false;
+        //        }
+        //    }
+        //}
+        static string yazdir(string metin, int indis, string yenideger)
+        {
+            metin = metin.Remove(indis, 2);
+            return metin.Insert(indis, yenideger + " ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            indis = rnd.Next(0, answers[currentQuestionIndex].Length);
+            if (previousGuesses.Count == 0)
+            {
+                indis *= 2;
+                previousGuesses.Add(indis);
+                harf = answers[currentQuestionIndex].Substring(indis / 2, 1);
+                label1.Text = yazdir(label1.Text, indis, harf);
+                score -= 10;
+                label2.Text = $"Score: {score}";
+            }
+            else
+            {
+                for (int i = 0; i < previousGuesses.Count; i++)
+                {
+                    if (previousGuesses[i] == indis * 2)
+                    {
+                        indis = rnd.Next(0, answers[currentQuestionIndex].Length);
+                        i = -1;
+                    }
+                }
+                indis *= 2;
+                previousGuesses.Add(indis);
+                harf = answers[currentQuestionIndex].Substring(indis / 2, 1);
+                label1.Text = yazdir(label1.Text, indis, harf);
+                score -= 10;
+                label2.Text = $"Score: {score}";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            score += 10 * (answers[currentQuestionIndex].Length - previousGuesses.Count());
+            previousGuesses.Clear();
+
+            if (currentQuestionIndex < questions.Length)
+            {
+                label3.Text = questions[currentQuestionIndex];
+                label2.Text = $"Score: {score}";
+            }
+            else
+            {
+                label3.Text = "Game Over!";
+                label2.Text = $"Final Score: {score}";
+            }
+            label1.Text = answers[currentQuestionIndex];
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            score -= 10 * (answers[currentQuestionIndex].Length - previousGuesses.Count());
+            
+            if (currentQuestionIndex < questions.Length)
+            {
+                label3.Text = questions[currentQuestionIndex];
+                label2.Text = $"Score: {score}";
+
+            }
+            else
+            {
+                
+                label3.Text = "Game Over!";
+                label2.Text = $"Final Score: {score}";
+            }
+        }
+        void surehak()
+        {
+            label4.Text ="KALAN SÜRE : "+ + Math.Round(zaman,1);
+            
+        }
+        void oyunBitti()
+        {
+            if (zaman <= 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("OYUN BİTTİ! KELİMEYİ BULAMADIN");
+
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            oyunBitti();
+            surehak();
+            if (zaman > 0)
+            {
+                zaman-=0.1;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            currentQuestionIndex++;
+            previousGuesses.Clear();
+
+            if (currentQuestionIndex < questions.Length)
+            {
+                label3.Text = questions[currentQuestionIndex];
+                label2.Text = $"Score: {score}";
+                labelaktar();
+                timer1.Start();
+
+            }
+            else
+            {
+
+                label3.Text = "Game Over!";
+                label2.Text = $"Final Score: {score}";
+            }
+        }
+    }
+}
